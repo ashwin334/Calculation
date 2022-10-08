c35f6920d5a5ecf5c5848a1aeaebecea49b9d2fb
ashwin333 <ashwinpatil832@gmail.com>
2022-10-08 22:07:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 9;
+            bool c = a == b;
+            Console.WriteLine(c);   
+
+            bool d = a == b;
+            Console.Write(d);
+        }
+    }
+}
+          
